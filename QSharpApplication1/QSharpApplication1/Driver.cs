862ca08641e3d7da18a1b0f16c42fbf051b78290
ChrisRodgers-code <60756647+ChrisRodgers-code@users.noreply.github.com>
2020-02-06 14:51:38 -0600

    Add files via upload

---
+++ QSharpApplication1/QSharpApplication1/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.QSharpApplication1
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            var estimator = new ResourcesEstimator();
+            BellTest.Run(estimator, 1000, Result.Zero).Wait();
+            System.Console.WriteLine(estimator.ToTSV());
+            /*using (var qsim = new QuantumSimulator())
+            {
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach(Result initial in initials)
+                {
+                    var res = BellTest.Run(qsim, 1000, initial).Result;
+                    var (numZeros, numOnes, agree) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
+                }
+            }*/
+            System.Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
